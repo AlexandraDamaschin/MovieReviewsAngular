04bc2eaad52ff99f8a01404b4ed3b81ef2853306
Eimutisg <eimutis.genys@gmail.com>
2018-02-23 12:49:43 +0000

    Modified String file

--- MovieReviewsFrontend/src/services/strings.ts
+++ MovieReviewsFrontend/src/services/strings.ts
@@ -1,4 +1,5 @@
 export class StringList{
     public static OMDB_API_BASE : string = "http://www.omdbapi.com/?apikey=60d77be4&plot=long&t=";
+    public static OMDB_API_BASE_ID : string = "http://www.omdbapi.com/?apikey=60d77be4&plot=long&i=";
     public static CUSTOM_API_BASE : string = "http://localhost:57581/api/reviews/"; 
 }
