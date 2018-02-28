e1779b4534fc31e978d82cbbe323e4d821e50d62
Josh Butler <31965183+S00161171@users.noreply.github.com>
2018-02-28 21:14:33 +0000

    push

--- MovieReviewsFrontend/src/app/app.component.ts
+++ MovieReviewsFrontend/src/app/app.component.ts
@@ -8,3 +8,16 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
 }
+
+
+var contosoChatHubProxy = $.connection.contosoChatHub;
+contosoChatHubProxy.client.addContosoChatMessageToPage = function (name, message) {
+    console.log(name + ' ' + message);
+};
+$.connection.hub.start().done(function () {
+    // Wire up Send button to call NewContosoChatMessage on the server.
+    $('#newContosoChatMessage').click(function () {
+         contosoChatHubProxy.server.newContosoChatMessage($('#displayname').val(), $('#message').val());
+         $('#message').val('').focus();
+     });
+});
