8b8e0769d1ad1d26f8aad42bdc78188a69422853
Eimutisg <eimutis.genys@gmail.com>
2018-03-29 10:43:09 +0100

    Updated String

--- MovieReviewsFrontend/src/services/strings.ts
+++ MovieReviewsFrontend/src/services/strings.ts
@@ -2,5 +2,5 @@ export class StringList{
     public static OMDB_API_BASE : string = "http://www.omdbapi.com/?apikey=60d77be4&plot=long&t=";
     public static OMDB_API_BASE_ID : string = "http://www.omdbapi.com/?apikey=60d77be4&plot=long&i=";
     // public static CUSTOM_API_BASE : string = "http://radprojapi.azurewebsites.net/api/reviews/"; 
-    public static CUSTOM_API_BASE : string = "http://localhost:63548/api/Reviews/"; 
+    public static CUSTOM_API_BASE : string = "http://localhost:63548/api/Reviews/GetReviews/"; 
 }
