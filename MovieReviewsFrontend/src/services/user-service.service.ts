469a88ada6273eff5f8bc6507e675ff738c9a50c
Eimutisg <eimutis.genys@gmail.com>
2018-04-02 11:35:34 +0100

    Revert "Merge branch 'Login' of https://github.com/AlexandraDamaschin/MovieReviewsAngular into Login"
    This reverts commit 472062e95f9ca9b8213f433006db574ca3ac6f3c, reversing
    changes made to 1d911dc24e8d81e3be3ba26e7102a501f3f40d3a.

--- MovieReviewsFrontend/src/services/user-service.service.ts
+++ MovieReviewsFrontend/src/services/user-service.service.ts
@@ -4,80 +4,72 @@ import { Http, Response, Headers, RequestOptions } from '@angular/http';
 
 import { Observable } from 'rxjs/Rx';
 import { BehaviorSubject } from 'rxjs/Rx';
-import { ConfigService } from '../services/config.service';
+import { ConfigService} from '../services/config.service';
 import { UserRegistration } from '../services/user.registration';
 
-import { BaseService } from "./base.service";
+import {BaseService} from "./base.service";
 
 //import '../../rxjs-operators';
 
+
+
 @Injectable()
 export class UserServiceService extends BaseService {
-
-  baseUrl: string = '';
-
-  private _authNavStatusSource = new BehaviorSubject<boolean>(false);
-
-  authNavStatus$ = this._authNavStatusSource.asObservable();
-
-  private loggedIn = false;
-
-  constructor(
-    private http: Http,
-    private configService: ConfigService) {
-    super();
-    this.loggedIn = !!localStorage.getItem('auth_token');
-
-    this._authNavStatusSource.next(this.loggedIn);
-    this.baseUrl = configService.getApiURI();
-  }
-
-  register(email: string, password: string, ConfirmPassword: string, UserName: string): Observable<UserRegistration> {
-    let body = JSON.stringify({ email, password, ConfirmPassword, UserName });
-    let headers = new Headers({ 'Content-Type': 'application/json' });
-    let options = new RequestOptions({ headers: headers });
-
-    return this.http.
-      post(this.baseUrl + "api/Account/Register",
-        JSON.stringify({ email, UserName, password, ConfirmPassword }), { headers }
-      )
-      .map(res => res.json())
-      .map(res => {
-        localStorage.setItem('auth_token', res.auth_token);
-        this.loggedIn = true;
-        this._authNavStatusSource.next(true);
-        return true;
-      })
-      .catch(this.handleError);
-  }
-
-  login(userName, password, ConfirmPassword) {
-    let headers = new Headers();
-    headers.append('Content-Type', 'application/json');
-
-    return this.http
-      .post(
+  
+  baseUrl: string = 'http://localhost:63548/';
+  
+    
+    private _authNavStatusSource = new BehaviorSubject<boolean>(false);
+    
+    authNavStatus$ = this._authNavStatusSource.asObservable();
+  
+    private loggedIn = false;
+  
+    constructor(private http: Http, private configService: ConfigService) {
+      super();
+      this.loggedIn = !!localStorage.getItem('auth_token');
+     
+      this._authNavStatusSource.next(this.loggedIn);
+      this.baseUrl = configService.getApiURI();
+    }
+  
+      register(email: string, password: string, ConfirmPassword: string, UserName: string): Observable<UserRegistration> {
+      let body = JSON.stringify({ email, password, ConfirmPassword, UserName });
+      let headers = new Headers({ 'Content-Type': 'application/json' });
+      let options = new RequestOptions({ headers: headers });
+  
+      return this.http.post(this.baseUrl + "api/Account/Register", body, options)
+        .map(res => true)
+        .catch(this.handleError);
+    }  
+  
+     login(userName, password, ConfirmPassword) {
+      let headers = new Headers();
+      headers.append('Content-Type', 'application/json');
+  
+      return this.http
+        .post(
         this.baseUrl + 'api/Account/AddExternalLogin',
-        JSON.stringify({ userName, password, ConfirmPassword }), { headers }
-      )
-      .map(res => res.json())
-      .map(res => {
-        localStorage.setItem('auth_token', res.auth_token);
-        this.loggedIn = true;
-        this._authNavStatusSource.next(true);
-        return true;
-      })
-      .catch(this.handleError);
+        JSON.stringify({ userName, password , ConfirmPassword}),{ headers }
+        )
+        .map(res => res.json())
+        .map(res => {
+          localStorage.setItem('auth_token', res.auth_token);
+          this.loggedIn = true;
+          this._authNavStatusSource.next(true);
+          return true;
+        })
+        .catch(this.handleError);
+    }
+  
+    logout() {
+      localStorage.removeItem('auth_token');
+      this.loggedIn = false;
+      this._authNavStatusSource.next(false);
+    }
+  
+    isLoggedIn() {
+      return this.loggedIn;
+    }  
   }
-
-  logout() {
-    localStorage.removeItem('auth_token');
-    this.loggedIn = false;
-    this._authNavStatusSource.next(false);
-  }
-
-  isLoggedIn() {
-    return this.loggedIn;
-  }
-}
-
+  
